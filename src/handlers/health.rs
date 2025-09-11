eae49d21d3a0a7fa1784eb73308bc0f4f25b2627
Ozan Kenan Güngör <ozankenangungor@Ozan-MacBook-Pro.local>
2025-09-11 22:09:51 +0300

    health update

--- src/handlers/health.rs
+++ src/handlers/health.rs
@@ -2,14 +2,5 @@ use axum::{http::StatusCode, response::IntoResponse, Json};
 use serde_json::json;
 
 pub async fn handler() -> impl IntoResponse {
-    (
-        StatusCode::OK,
-        Json(json!({
-            "ozan": "kenan",
-            "asli":"sahin",
-            "123":"456",
-            "anne seni cok seviyorum":"anne seni cok seviyorum",
-            "KAŞIK":"ÇATAL"
-        })),
-    )
+    (StatusCode::OK, "VERSION 3 - DEPLOYMENT BAŞARILI")
 }
