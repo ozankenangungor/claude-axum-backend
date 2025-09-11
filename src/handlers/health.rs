ef8a5bdc7416283888d8a0d304df57ff54667158
Ozan Kenan Güngör <ozankenangungor@Ozan-MacBook-Pro.local>
2025-09-11 22:22:25 +0300

    health update

--- src/handlers/health.rs
+++ src/handlers/health.rs
@@ -1,5 +1,4 @@
 use axum::{http::StatusCode, response::IntoResponse, Json};
-use serde_json::json;
 
 pub async fn handler() -> impl IntoResponse {
     (StatusCode::OK, "VERSION 3 - DEPLOYMENT BAŞARILI")
